8c2522c32c579c4313ec8f0d9491d95a26e51700
phreaker56 <92488891+phreaker56@users.noreply.github.com>
2024-07-05 17:10:50 -0600

    Update Dork.sh

--- Dork.sh
+++ Dork.sh
@@ -1,32 +1,34 @@
-
 #!/bin/bash
 
-# Función para buscar un dork específico en Google
 search_dork() {
-  local dork="$1"
-  local query=$(echo "$dork" | sed 's/ /+/g' | sed 's/"/%22/g')
-  local url="https://www.google.com/search?q=${query}"
-  echo "URL being accessed: $url" # Diagnosticar la URL
+    local dork="$1"
+    local query=$(echo "$dork" | sed 's/ /+/g' | sed 's/"/%22/g')
+    local url="https://www.google.com/search?q=${query}"
+    echo "Accessing URL: $url"
 
-  response=$(curl -s -A "Mozilla/5.0" "$url")
-  
-  echo "Curl Response: " # Diagnostico para ver la respuesta inicial de curl
-  echo "$response" | head -n 5 # Muestra las primeras 5 líneas de la respuesta
+    # Realizar la solicitud HTTP con curl y guardar la respuesta y el código de estado HTTP
+    response=$(curl -s -A "Mozilla/5.0" -w "%{http_code}" -o temp.html "$url")
+    http_code="${response: -3}"  # Los últimos 3 caracteres son el código de estado HTTP
 
-  # Filtrar y mostrar los resultados relevantes
-  echo "$response" | grep -oP '(?<=<a href="/url\?q=)(.*?)(?=&amp;sa=U)' | while read -r link ; do
-    echo "URL: $link"
-  done
+    if [ "$http_code" -eq "200" ]; then
+        echo "Successful HTTP request. Parsing..."
+        cat temp.html | grep -oP '(?<=<a href="/url\?q=)(.*?)(?=&amp;sa=U)' | while read -r link ; do
+          echo "URL: $link"
+        done
+    else
+        echo "Failed HTTP request. Status Code: $http_code"
+    fi
+    rm temp.html  # Limpiar archivo temporal
 }
 
 dorks=(
-  'intext:"powered by Adyen" "buy toys" inurl:checkout',
-  'intext:"powered by Adyen" "juguetes" inurl:checkout',
-  'intext:"powered by Adyen" "toy store" inurl:cart'
+    'intext:"powered by Adyen" "buy toys" inurl:checkout',
+    'intext:"powered by Adyen" "juguetes" inurl:checkout',
+    'intext:"powered by Adyen" "toy store" inurl:cart'
 )
 
 for dork in "${dorks[@]}"; do
-  echo "Results for dork: $dork"
-  search_dork "$dork"
-  echo "=================================================="
+    echo "Results for dork: $dork"
+    search_dork "$dork"
+    echo "=================================================="
 done
